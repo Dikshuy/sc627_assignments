45bf54916fc67edfd7e1c7b718f58d4e30d88fcb
Dikshuy <dikshant1212hsr@gmail.com>
2022-03-05 18:30:59 +0530

    ros implementation of potential path planner done

--- assignment_2/scripts/potential_function_planner.py
+++ assignment_2/scripts/potential_function_planner.py
@@ -12,25 +12,29 @@ import matplotlib.pyplot as plt
 
 
 def forward(curr_pos, grad, step_size, goal):
-    # a, b = goal[0]-curr_pos[0], goal[1]-curr_pos[1]
-    # norm = math.sqrt(a**2+b**2)
     return [curr_pos[0]+grad[0]*step_size, curr_pos[1]+grad[1]*step_size]
 
-# def update(curr_pos, new_pos, client):
-#     wp = MoveXYGoal()
-#     wp.pose_dest.x = new_pos[0]
-#     wp.pose_dest.y = new_pos[1]
-#     wp.pose_dest.theta = math.atan2((new_pos[1]-curr_pos[1]),(new_pos[0]-curr_pos[0]))
-#     # send waypoint to turtlebot3 via move_xy server
-#     client.send_goal(wp)
-#     client.wait_for_result()
+def plotter(curr_pos, goal, start_time, time_elapsed, total_distance, step_size, distance_from_goal):
+    time_elapsed.append(time.time()-start_time)
+    total_distance += step_size
+    distance_from_goal.append(dist(curr_pos, goal))
+    return time_elapsed, total_distance, distance_from_goal
 
-#     # getting updated robot location
-#     result = client.get_result()
+def update(curr_pos, new_pos, client):
+    wp = MoveXYGoal()
+    wp.pose_dest.x = new_pos[0]
+    wp.pose_dest.y = new_pos[1]
+    wp.pose_dest.theta = math.atan2((new_pos[1]-curr_pos[1]),(new_pos[0]-curr_pos[0]))
+    # send waypoint to turtlebot3 via move_xy server
+    client.send_goal(wp)
+    client.wait_for_result()
 
-#     # write to output file (replacing the part below)
-#     print(result.pose_final.x, result.pose_final.y, result.pose_final.theta)
-#     return [result.pose_final.x, result.pose_final.y]
+    # getting updated robot location
+    result = client.get_result()
+
+    # write to output file (replacing the part below)
+    print(result.pose_final.x, result.pose_final.y, result.pose_final.theta)
+    return [result.pose_final.x, result.pose_final.y]
 
 def attractive_potential(curr_pos, goal, d_star):
     d = dist(curr_pos, goal)
@@ -68,8 +72,17 @@ def repulsive_potential_gradient(curr_pos, obstaclesList, q_star, eta):
     return grad
 
 def potential(start, goal, step_size, obstaclesList):
+    rospy.init_node('test', anonymous= True)
+    client = actionlib.SimpleActionClient('move_xy', MoveXYAction)
+    client.wait_for_server()
+
     potential = 0 
     d_star, q_star = 2, 2
+    total_distance = 0 
+    time_elapsed = []
+    distance_from_goal = []
+    start_time = time.time()
+
     curr_pos = start
     path = [start]
     while dist(curr_pos, goal) > step_size:
@@ -82,24 +95,27 @@ def potential(start, goal, step_size, obstaclesList):
 
         last_pos = curr_pos
         curr_pos = forward(curr_pos, grad, step_size, goal)
-        # curr_pos = update(last_pos, curr_pos, client)
+        curr_pos = update(last_pos, curr_pos, client)
 
         ###
-        att = attractive_potential(curr_pos, goal, d_star)
-        rep = repulsive_potential(curr_pos, obstaclesList, q_star)
-        print("potential=", att+rep)
+        # att = attractive_potential(curr_pos, goal, d_star)
+        # rep = repulsive_potential(curr_pos, obstaclesList, q_star)
+        # print("potential=", att+rep)
         ###
 
         path.append(curr_pos)
-        # print(path)
-        print()
-        # print(grad_y, grad_x)
-        path.append(curr_pos)
+        time_elapsed, total_distance, distance_from_goal = plotter(curr_pos, goal, start_time, time_elapsed, total_distance, step_size, distance_from_goal)
 
     path.append(goal)
     write(path)
-    # print(path)
     print("Success")
+    print("total time time_elapsed:", round(time_elapsed[-1], 5))
+    print("total distance covered:", round(total_distance, 3))
+    # plt.title("Distance from goal vs time for potential path planner algo")
+    # plt.xlabel("Time")
+    # plt.ylabel("Distance from goal")
+    # plt.plot(time_elapsed, distance_from_goal, color = "green")
+    # plt.show()
     return path
 
 if __name__ == '__main__':
