f4204295d0a560888149d32eaf5d0aed427f4e29
Dikshuy <dikshant1212hsr@gmail.com>
2022-04-08 14:37:56 +0530

    assignment 4 code done

--- assignment_4/scripts/balancing.py
+++ assignment_4/scripts/balancing.py
@@ -10,24 +10,6 @@ import math
 ANG_MAX = math.pi/18
 VEL_MAX = 0.15
 
-#original robot data
-robot_pos = [0,0]
-vel = [0,0] 
-ang_vel = 0 
-
-#left robot data
-left_robot_pos = [0,0]
-left_vel = 0        
-
-#right robot data
-right_robot_pos = [0,0]
-right_vel = 0 
-
-# yaw data
-yaw = 0
-yaw_left = 0 
-yaw_right = 0
-
 def dist(p1,p2):
     return math.sqrt((p1[1]-p2[1])**2+(p1[0]-p2[0])**2)
 
@@ -37,13 +19,9 @@ def velocity_convert(x, y, theta, vel_x, vel_y):
     Velocity vector (vel_x, vel_y)
     '''
 
-    gain_ang = 1 #modify if necessary
-    
-    ang = math.atan2(vel_y, vel_x)
-    ang_err = min(max(ang - theta, -ANG_MAX), ANG_MAX)
-
-    v_lin =  min(max(math.cos(ang_err) * math.sqrt(vel_x ** 2 + vel_y ** 2), -VEL_MAX), VEL_MAX)
-    v_ang = gain_ang * ang_err
+    v_lin =  min(max(vel_x, - VEL_MAX), VEL_MAX)
+    v_ang = 0
+    print(v_lin,v_ang)
     return v_lin, v_ang
 
 def callback_odom(data):
@@ -53,9 +31,8 @@ def callback_odom(data):
     # print(data)
     robot_pos[0] = data.pose.pose.position.x 
     robot_pos[1] = data.pose.pose.position.y 
-    vel[0] = data.twist.twist.linear.x 
-    vel[1] = data.twist.twist.linear.y
-    roll, pitch, yaw = euler_from_quaternion([data.pose.pose.orientation.x, data.pose.pose.orientation.y, data.pose.pose.orientation.z, data.pose.orientation.w])
+    vel = data.twist.twist.linear.x 
+    roll, pitch, yaw = euler_from_quaternion([data.pose.pose.orientation.x, data.pose.pose.orientation.y, data.pose.pose.orientation.z, data.pose.pose.orientation.w])
     ang_vel = data.twist.twist.angular.z
     return robot_pos, vel, yaw, ang_vel
 
@@ -68,7 +45,7 @@ def callback_left_odom(data):
     left_robot_pos[0] = data.pose.pose.position.x 
     left_robot_pos[1] = data.pose.pose.position.y 
     left_vel = data.twist.twist.linear.x 
-    roll, pitch, yaw_left = euler_from_quaternion([data.pose.pose.orientation.x, data.pose.pose.orientation.y, data.pose.pose.orientation.z, data.pose.orientation.w])
+    roll, pitch, yaw_left = euler_from_quaternion([data.pose.pose.orientation.x, data.pose.pose.orientation.y, data.pose.pose.orientation.z, data.pose.pose.orientation.w])
     return left_robot_pos, left_vel, yaw_left
 
 def callback_right_odom(data):
@@ -77,13 +54,10 @@ def callback_right_odom(data):
     '''
     print('right robot')
     # print(data)
-
-    right_robot_pos = [0,0]
-    right_vel = 0 
     right_robot_pos[0] = data.pose.pose.position.x 
     right_robot_pos[1] = data.pose.pose.position.y 
     right_vel = data.twist.twist.linear.x 
-    roll, pitch, yaw_right = euler_from_quaternion([data.pose.pose.orientation.x, data.pose.pose.orientation.y, data.pose.pose.orientation.z, data.pose.orientation.w])
+    roll, pitch, yaw_right = euler_from_quaternion([data.pose.pose.orientation.x, data.pose.pose.orientation.y, data.pose.pose.orientation.z, data.pose.pose.orientation.w])
     return right_robot_pos, right_vel, yaw_right 
 
 def balancing():
@@ -94,26 +68,46 @@ def balancing():
 
     pub_vel = rospy.Publisher('/cmd_vel', Twist, queue_size = 10)
     r = rospy.Rate(30)
-
-    while not rospy.is_shutdown(): 
-        u = dist(robot_pos, right_robot_pos)-dist(robot_pos, left_robot_pos)
-        v_x = 0.8*u
+    itr = 0
+    while itr < 1000: 
         #calculate v_x, v_y as per the balancing strategy
+        u = dist(robot_pos, right_robot_pos)-dist(robot_pos, left_robot_pos)
+        # v_x = 0.8*u        # P controller
+        # print(v_x)
         #Make sure your velocity vector is feasible (magnitude and direction)
-
+        # if vel > VEL_MAX or right_vel > VEL_MAX or left_vel > VEL_MAX:
+        #     break
         #convert velocity vector to linear and angular velocties using velocity_convert function given above
-        v_lin, v_ang = velocity_convert(robot_pos[0], robot_pos[1], yaw, v_x, 0)
+        v_lin, v_ang = velocity_convert(robot_pos[0], robot_pos[1], yaw, u, 0.0)
         #publish the velocities below
         vel_msg = Twist()
         vel_msg.linear.x = v_lin
         vel_msg.angular.z = v_ang
         pub_vel.publish(vel_msg)
-        print(v_lin, v_ang)
         #store robot path with time stamps (data available in odom topic)
-
+        itr += 1
         r.sleep()
 
 if __name__ == '__main__':
+
+    #original robot data
+    robot_pos = [0,0]    # x, y position of the bot
+    vel = 0              # velcoity of the bot
+    ang_vel = 0          # angular velocity of the bot
+
+    #left robot data
+    left_robot_pos = [0,0]
+    left_vel = 0        
+
+    #right robot data
+    right_robot_pos = [0,0]
+    right_vel = 0 
+
+    # yaw data
+    yaw = 0
+    yaw_left = 0 
+    yaw_right = 0
+
     try:
         balancing()
     except rospy.ROSInterruptException:
